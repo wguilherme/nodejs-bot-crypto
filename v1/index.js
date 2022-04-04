b2dbd4cbf0b5c7ad694964537d09eb15ec34ff98
W. Guilherme <wguilhermecriativo@gmail.com>
2022-04-04 11:05:16 -0300

    refactor: named variables, decouple parameters

--- v1/index.js
+++ v1/index.js
@@ -1,16 +1,21 @@
+const SYMBOL = "BTCBUSD";
+const BINANCE_INTERVAL = "1m"
+const WATCH_INTERVAL = 1000;
+
 async function process() {
+  const maxPrice = 36291
+  const minPrice = 35764
   const axios = require("axios");
-  const response = await axios.get("https://api.binance.com/api/v3/klines?symbol=BTCBUSD&interval=1m");
-  const candle = response.data[499];
-  const price = parseFloat(candle[4]);
-  if (price >= 36291)
-    console.log("Vender!");
-  else if (price <= 35764)
-    console.log("Comprar!");
-  else
-    console.log("Aguardar...");
+  const response = await axios.get(`https://api.binance.com/api/v3/klines?symbol=${SYMBOL}&interval=${BINANCE_INTERVAL}`);
+  // get last position of array
+  const candleLastPosition = response.data.length - 1;
+  const candlePrice = parseFloat(candleLastPosition[4]);
+
+  if (candlePrice >= maxPrice) console.log("Vender!");
+  else if (candlePrice <= minPrice) console.log("Comprar!");
+  else console.log("Aguardar...");
 }
 
-setInterval(process, 1000);
+setInterval(process, WATCH_INTERVAL);
 
 process();
